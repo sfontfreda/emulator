cf721ddd5c9f76884ca71e7a29ace63a86c54031
Sílvia Fontfreda i Casals <hello@sealvia.dev>
2026-03-09 18:52:58 +0100

    feat: refactor to HomeScreen class

--- main.py
+++ main.py
@@ -1,47 +1,21 @@
 import pygame
-import sys
 import config
+from ui.screens.home_screen import HomeScreen
 from core import game_scanner
-from core.emulator import launch
 
 def main():
-    roms = game_scanner.get_roms()
-    print(roms)
     pygame.init()
-    
     screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
     pygame.display.set_caption("pokemachine")
     clock = pygame.time.Clock()
-    selected_index = 0
+    roms = game_scanner.get_roms()
+    home = HomeScreen(roms)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    launch(roms[selected_index])
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-                if event.key == pygame.K_UP:
-                    if selected_index > 0: 
-                        selected_index -= 1
-                if event.key == pygame.K_DOWN:
-                    if selected_index < len(roms) -1:
-                        selected_index += 1
-
+        events = pygame.event.get()
+        home.handle_events(events)
         screen.fill(config.BLACK)
-
-        font = pygame.font.Font(None, 32)
-       
-        
-        for i, rom in enumerate(roms):
-            color = config.GREEN if i == selected_index else config.WHITE
-            text = font.render(rom["name"], True, color)
-            screen.blit(text, (10, 10 + i * 40))
-
+        home.draw(screen)
         pygame.display.flip()
         clock.tick(config.FPS)
 
---
+++ ui/screens/home_screen.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+from core import game_scanner
+from core.emulator import launch
+import config
+
+
+class HomeScreen: 
+  def __init__(self, roms):
+    self.roms = roms
+    self.selected_index = 0
+    self.font = pygame.font.Font(None, 32)
+
+  def handle_events(self, events):
+    for event in events:
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RETURN:
+          launch(self.roms[self.selected_index])
+        if event.key == pygame.K_ESCAPE:
+          pygame.quit()
+          sys.exit()
+        if event.key == pygame.K_UP:
+          if self.selected_index > 0: 
+              self.selected_index -= 1
+        if event.key == pygame.K_DOWN:
+          if self.selected_index < len(self.roms) -1:
+            self.selected_index += 1
+
+  def draw(self, screen):
+    for i, rom in enumerate(self.roms):
+        color = config.GREEN if i == self.selected_index else config.WHITE
+        text =  self.font.render(rom["name"], True, color)
+        screen.blit(text, (10, 10 + i * 40))
